69f93ffcf55d3f69c56d4e4d5f86f490f50f8fd5
Alamin-git <alamin2.mas@gmail.com>
2022-06-07 18:05:49 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        myTheme: {
+          primary: "#22d3ee",
+
+          secondary: "#D926A9",
+
+          accent: "#1FB2A6",
+
+          neutral: "#191D24",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+};
