64afffe6625001e74554552881ba898bcf0d5ece
Alamin-git <alamin2.mas@gmail.com>
2022-06-07 19:54:37 +0600

    navbar route add & pages added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 import About from "./pages/About/About";
+import Appointment from "./pages/Appointment/Appointment";
+import ContactUs from "./pages/ContactUs/ContactUs";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Reviews from "./pages/Reviews/Reviews";
 import Header from "./pages/Shared/Navbar/Header";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header></Header>
       <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/about" element={<About/>}></Route>☻
+        <Route path="/" element={<Home/>}/>
+        <Route path="about" element={<About/>}/>
+        <Route path="appointment" element={<Appointment/>}/>
+        <Route path="reviews" element={<Reviews/>}/>
+        <Route path="contactUs" element={<ContactUs/>}/>
+        <Route path="login" element={<Login/>}/>
       </Routes>
     </div>
   );
--- src/pages/Shared/Navbar/Header.js
+++ src/pages/Shared/Navbar/Header.js
@@ -1,8 +1,32 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
+   const menuItems = (
+      <>
+         <li>
+            <Link to="/">Home</Link>
+         </li>
+         <li>
+            <Link to="about">About</Link>
+         </li>
+         <li>
+            <Link to="appointment">Appointment</Link>
+         </li>
+         <li>
+            <Link to="reviews">Reviews</Link>
+         </li>
+         <li>
+            <Link to="contactUs">Contact Us</Link>
+         </li>
+         <li>
+            <Link to="login">Login</Link>
+         </li>
+      </>
+   );
    return (
       <div className="navbar bg-base-100">
          <div className="navbar-start">
-            <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+            <a className="btn btn-ghost normal-case text-xl">Doctors Portal</a>
             <div className="dropdown">
                <label tabindex="0" className="btn btn-ghost lg:hidden">
                   <svg
@@ -24,72 +48,12 @@ const Header = () => {
                   tabindex="0"
                   className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
                >
-                  <li>
-                     <a>Item 1</a>
-                  </li>
-                  <li tabindex="0">
-                     <a className="justify-between">
-                        Parent
-                        <svg
-                           className="fill-current"
-                           xmlns="http://www.w3.org/2000/svg"
-                           width="24"
-                           height="24"
-                           viewBox="0 0 24 24"
-                        >
-                           <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                        </svg>
-                     </a>
-                     <ul className="p-2">
-                        <li>
-                           <a>Submenu 1</a>
-                        </li>
-                        <li>
-                           <a>Submenu 2</a>
-                        </li>
-                     </ul>
-                  </li>
-                  <li>
-                     <a>Item 3</a>
-                  </li>
+                  {menuItems}
                </ul>
             </div>
          </div>
-         <div className="navbar-end">
-         
-            <div className=" hidden lg:flex">
-               <ul className="menu menu-horizontal p-0">
-                  <li>
-                     <a>Item 1</a>
-                  </li>
-                  <li tabindex="0">
-                     <a>
-                        Parent
-                        <svg
-                           className="fill-current"
-                           xmlns="http://www.w3.org/2000/svg"
-                           width="20"
-                           height="20"
-                           viewBox="0 0 24 24"
-                        >
-                           <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                        </svg>
-                     </a>
-                     <ul className="p-2">
-                        <li>
-                           <a>Submenu 1</a>
-                        </li>
-                        <li>
-                           <a>Submenu 2</a>
-                        </li>
-                     </ul>
-                  </li>
-                  <li>
-                     <a>Item 3</a>
-                  </li>
-               </ul>
-            </div>
-            <a className="btn">Get started</a>
+         <div className="navbar-end hidden lg:flex">
+            <ul className="menu menu-horizontal p-0">{menuItems}</ul>
          </div>
       </div>
    );
